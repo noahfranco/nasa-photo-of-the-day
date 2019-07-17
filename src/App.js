2aac1ae26037b87e69ae15845d8463b27b6dc82f
noahfranco <noahfra@gmail.com>
2019-07-17 17:33:32 -0400

    finished adding hook and looped it

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React, {useEffect, useState} from "react";
 import "./App.css";
 
-import axios from "./axios"; 
+import axios from "axios"; 
 
-const [NasaData, setNasaData] = useState([]); 
 
-useEffect(() => {
-  axios
-  .get("https://api.nasa.gov/neo/rest/v1/neo/3542519?api_key=DEMO_KEY")
-  .then((element) => {
-  return element.data.links 
-  })
-}, []); 
+
+
 
 function App() {
+const [NasaData, setNasaData] = useState(""); 
+
+  useEffect(() => {
+    axios
+    .get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2019-07-15")
+    .then((element) => {
+    return setNasaData(element.data.copyright) 
+    })
+  },[]); 
+
   return (
     <div className="App">
       <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
+        {NasaData}
       </p>
     </div>
   );
